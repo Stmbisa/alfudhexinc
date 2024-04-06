a726e8c2f8cb051823e84be810a39adf1c2b1c87
Mbisa <jcmbisa@gmail.com>
2024-04-06 07:28:24 +0300

    story ..

---
+++ src/app/api/translations/[id]/route.js
@@ -0,0 +1,30 @@
+import { Translation } from "@/lib/models";
+import { connectToDb } from "@/lib/utils";
+import { NextResponse } from "next/server";
+import middleware from '@/lib/middleware';
+    
+export const PUT = middleware(async (request, { params }) => {
+  const { id } = params;
+
+  try {
+    connectToDb();
+
+    // Get update data from request body
+    const updateData = JSON.parse(request.body);
+
+    const updatedTranslation = await Translation.findByIdAndUpdate(
+      id,
+      updateData,
+      { new: true }
+    );
+
+    if (!updatedTranslation) {
+      return NextResponse.json({ error: 'Translation not found' }, { status: 404 });
+    }
+
+    return NextResponse.json(updatedTranslation);
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to update translation!");
+  }
+});
