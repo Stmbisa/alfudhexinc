d2d0867916896a477b5fc839378374110f61c8f6
Mbisa <jcmbisa@gmail.com>
2024-04-09 19:15:01 +0300

    export identifier error

--- src/lib/middleware.js
+++ src/lib/middleware.js
@@ -3,7 +3,7 @@ import { connectToDb } from "@/lib/utils";
 import { getUserIdFromRequest } from "@/lib/auth";
 import { User } from "./models";
 
-export default async function middleware(request) {
+export async function middleware(request) {
   const { pathname, method } = request;
 
   // protecting POST, PUT, DELETE routes
@@ -16,20 +16,18 @@ export default async function middleware(request) {
         return NextResponse.redirect(new URL('/login', request.url));
     }
 
-    // 3. (Optional but recommended) Fetch user details for role-based authorization
+    // 3. Fetch user details for role-based authorization
     const user = await User.findById(userId);
 
-    // 3.1 (Optional) Check for admin role
+    // 3.1 Check for admin role
     if (!user.isAdmin && requiresAdmin(pathname)) {
       return NextResponse.json({ message: 'Unauthorized access' }, { status: 401 });
     }
-
   }
   return NextResponse.next();
 }
 
-
 // Helper function to determine admin-only routes
-export default async function requiresAdmin(pathname) {
-  return pathname.startsWith('/api/admin') //|| ... ;
+export function requiresAdmin(pathname) {
+  return pathname.startsWith('/api/admin'); // Add other admin paths as needed
 }
